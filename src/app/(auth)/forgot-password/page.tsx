577d3f641b8251f5c8be21e1cb87749125b3cb85
Bayram <byrm.telli.contact@gmail.com>
2024-07-31 18:22:27 +0300

    layout implemenetations

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>this is forgot password page.</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/product/[productId]/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ProductDetailLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>
+  {children}
+  <h2>Features products</h2>
+  <h1>product layout has rendered.</h1>
+  {/*carousal here*/}
+  </>;
+}
